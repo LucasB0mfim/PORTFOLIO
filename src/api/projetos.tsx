5d609d71d7cf07c1f8f938fc50b16e3202e954b3
lucas <lucasbomfimsobral@gmail.com>
2024-11-01 10:14:18 -0300

    Update: Add new project

--- src/api/projetos.tsx
+++ src/api/projetos.tsx
@@ -1,24 +1,24 @@
 const projetos = [
   {
     id: 1,
-    name: 'Efood',
-    description:
-      'O Efood é um projeto que aproxima clientes, restaurantes e entregadores de forma simples e prática. Para proporcionar uma experiência incrível para todo mundo, sua entrega vai muito além do delivery.',
-    url: 'https://efood-fast.vercel.app/'
-  },
-  {
-    id: 2,
     name: 'Eplay Games',
     description:
       'Eplay é um projeto que conecta jogadores aos melhores jogos digitais de forma simples e prática. Seu objetivo é Unir qualidade com praticidade.',
     url: 'https://lojaeplaygames.vercel.app/'
   },
   {
-    id: 3,
-    name: 'Organiza Tudo',
+    id: 2,
+    name: 'Encurta',
     description:
-      'Organiza Tudo foi criado para auxiliar as pessoas na gestão de seus compromissos e atividades, promovendo uma rotina mais organizada e eficiente.',
-    url: 'https://organiza-tudo.vercel.app/'
+      'O Encurta é um site que reduz o tamanho de qualquer link enviado pelo usuário, gerando uma versão mais curta e visualmente limpa para quem o receber.',
+    url: 'https://encurta-url.vercel.app/'
+  },
+  {
+    id: 3,
+    name: 'Efood',
+    description:
+      'O Efood é um projeto que aproxima clientes, restaurantes e entregadores de forma simples e prática. Para proporcionar uma experiência incrível para todo mundo, sua entrega vai muito além do delivery.',
+    url: 'https://efood-fast.vercel.app/'
   }
 ]
 
